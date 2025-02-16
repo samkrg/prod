0fe01d47f61d274efb72a003d0e18282fe2915fa
Chaitu3892 <chaitanya.mechengineer@gmail.com>
2025-02-16 11:17:01 +0530

    git_commands file added

---
+++ hr.sh
@@ -0,0 +1,7 @@
+Strategic Planning and Workforce Planning
+Recruitment & selection
+Performance management
+Learning & development
+Succession planning
+Compensation and benefits
+HR data, Analytics and HRIS
---
+++ prod.java
@@ -0,0 +1,4 @@
+public class SimpleJavaExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }}
